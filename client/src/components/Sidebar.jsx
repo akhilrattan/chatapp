5d7d56feed3785ef831ee0a9cc68e5a39efde7ca
akhilrattan <akhilrattan3@gmail.com>
2025-09-10 18:50:45 +0530

    container

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import assets from '../assets/assets' // Adjust path if needed
+import assets, { userDummyData } from '../assets/assets' // Adjust path if needed
+import { set } from 'mongoose'
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
   const navigate = useNavigate()
@@ -24,8 +25,28 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
                 <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search user...' />
             </div>
         </div>
+        <div className='flex flex-col'>
+            {userDummyData.map((user, index)=>(
+                <div onClick={()=>{setSelectedUser(user)}}
+                key={index} className={`relative flex items-center bg-transparent gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?.id === user.id && 'bg-[#4A47A3]/50'}`}>
+                    <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full' />
+                    <div className='flex flex-col leading-5'>
+                        <p>{user.fullName}</p>
+                        {
+                            index<3
+                            ? <span className='text-green-400 text-xs'>online</span>
+                            : <span className='text-neutral-400 text-xs'>offline</span>
+                        }
+                    </div>
+                    {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>
+                    {index}</p>
+                    }
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
 
 export default Sidebar
+ 
